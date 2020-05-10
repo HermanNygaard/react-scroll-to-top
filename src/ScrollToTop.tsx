562728be44c42059daaef73ae31f41ba37d9bbda
Herman Nygaard <hermannyg@gmail.com>
2020-05-10 16:10:02 +0200

    add button and functionality

---
+++ src/ScrollToTop.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+
+type Props = {
+  top?: number;
+  left?: boolean;
+  right?: boolean;
+  backgroundColor?: string;
+  color?: string;
+};
+
+function scrollToTop() {
+  document.documentElement.scrollTop = 0;
+}
+
+const ScrollToTop: React.FC<Props> = ({
+  top = 20,
+  color = "black",
+  backgroundColor = "white",
+}) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("scroll", onScroll);
+    // Remove listener on unmount
+    return () => document.removeEventListener("scroll", onScroll);
+  });
+
+  const style = {
+    bottom: "40px",
+    right: "40px",
+    position: "fixed",
+    marginLeft: "-50px",
+    zIndex: 2,
+    cursor: "pointer",
+    opacity: "0.4",
+    color: color,
+    border: "none",
+    backgroundColor: backgroundColor,
+    borderRadius: 15,
+    width: 40,
+    transition: "bottom 0.3s ease-in-out",
+    height: 40,
+    boxShadow: "0 9px 25px 0 rgba(132,128,177,0.28)",
+  } as React.CSSProperties;
+
+  console.log("rerender");
+  const onScroll = () => {
+    setVisible(document.documentElement.scrollTop > top);
+    console.log("scrolling", document.documentElement.scrollTop > top);
+  };
+
+  return (
+    <>
+      {visible && (
+        <button style={style} onClick={scrollToTop}>
+          <svg fill="black" viewBox="0 0 256 256">
+            <path d="M222.138,91.475l-89.6-89.6c-2.5-2.5-6.551-2.5-9.051,0l-89.6,89.6c-2.5,2.5-2.5,6.551,0,9.051s6.744,2.5,9.244,0L122,21.85  V249.6c0,3.535,2.466,6.4,6,6.4s6-2.865,6-6.4V21.85l78.881,78.676c1.25,1.25,2.992,1.875,4.629,1.875s3.326-0.625,4.576-1.875  C224.586,98.025,224.638,93.975,222.138,91.475z"></path>
+          </svg>
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ScrollToTop;
