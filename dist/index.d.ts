d7201814a506034715e1165c88b0a7db41a7ef6e
Herman Nygaard <hermannyg@gmail.com>
2020-07-09 22:58:04 +0200

    bundle css and add example

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-declare type Props = {
-    top?: number;
-    smooth?: boolean;
-};
-declare const ScrollToTop: React.FC<Props & React.HTMLAttributes<HTMLButtonElement>>;
-export default ScrollToTop;
