52da42f9b6a11cf27a47f090cf35712cd9605ae0
Herman Nygaard <hermannyg@gmail.com>
2020-07-15 15:41:42 +0200

    add custom svg path and viewbox

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,16 @@ import styles from "./styles.css";
 type Props = {
   top?: number;
   smooth?: boolean;
+  svgPath?: string;
+  viewBox?: string;
 };
 
 function scrollToTop(smooth: boolean = false) {
   if (smooth) {
-    try {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    } catch (e) {
-      document.documentElement.scrollTop = 0;
-    }
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   } else {
     document.documentElement.scrollTop = 0;
   }
@@ -27,9 +25,11 @@ const ScrollToTop: React.FC<
   Props & React.HTMLAttributes<HTMLButtonElement>
 > = ({
   top = 20,
-  className = "",
+  className = styles["scroll-to-top"],
   color = "black",
   smooth = false,
+  viewBox = "0 0 256 256",
+  svgPath = "M222.138,91.475l-89.6-89.6c-2.5-2.5-6.551-2.5-9.051,0l-89.6,89.6c-2.5,2.5-2.5,6.551,0,9.051s6.744,2.5,9.244,0L122,21.85  V249.6c0,3.535,2.466,6.4,6,6.4s6-2.865,6-6.4V21.85l78.881,78.676c1.25,1.25,2.992,1.875,4.629,1.875s3.326-0.625,4.576-1.875  C224.586,98.025,224.638,93.975,222.138,91.475z",
   ...props
 }) => {
   const [visible, setVisible] = useState(false);
@@ -47,12 +47,12 @@ const ScrollToTop: React.FC<
     <>
       {visible && (
         <button
-          className={`${className ? className : styles["scroll-to-top"]}`}
+          className={className}
           onClick={() => scrollToTop(smooth)}
           {...props}
         >
-          <svg fill={color} viewBox="0 0 256 256">
-            <path d="M222.138,91.475l-89.6-89.6c-2.5-2.5-6.551-2.5-9.051,0l-89.6,89.6c-2.5,2.5-2.5,6.551,0,9.051s6.744,2.5,9.244,0L122,21.85  V249.6c0,3.535,2.466,6.4,6,6.4s6-2.865,6-6.4V21.85l78.881,78.676c1.25,1.25,2.992,1.875,4.629,1.875s3.326-0.625,4.576-1.875  C224.586,98.025,224.638,93.975,222.138,91.475z"></path>
+          <svg fill={color} viewBox={viewBox}>
+            <path d={svgPath}></path>
           </svg>
         </button>
       )}
